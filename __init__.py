2d87cacf460a77e1bad19280c5b95e98d6348487
dhesenkamp <dhesenkamp@posteo.de>
2025-05-05 16:52:05 +0200

    feat/refac: update __init__.py to reflect reorganization of files (moved older agent setups into archive)

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
-from .orchestrator import OrchestratorAgent
-from .weather import WeatherAgent
+from .activities_agent import activitiesExecutor
+from .calendar_agent import calendarExecutor
+from .weather_agent import weatherExecutor
+
+from .archive.orchestrator import OrchestratorAgent
+from .archive.weather import WeatherAgent
