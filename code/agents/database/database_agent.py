c81ab3cce4f3063d019a7eafe448c6a82888bfcf
dhesenkamp <dhesenkamp@posteo.de>
2025-05-15 15:46:22 +0200

    feat: create database agent

---
+++ code/agents/database/database_agent.py
@@ -0,0 +1,28 @@
+import os
+import supabase
+import getpass
+
+from agents.base_agent import BaseAgent
+from agents.database.prompt_template import QUERY_PROMPT
+from agents.database.tools import queryDatabase
+
+TOOLS = [queryDatabase]
+
+
+class DatabaseAgent(BaseAgent):
+  def __init__(self, apiKey: str, tools: list = TOOLS, promptTemplate: str | None = QUERY_PROMPT):
+
+    super().__init__(apiKey=apiKey, tools=tools, promptTemplate=promptTemplate)
+
+
+if __name__ == "__main__":
+  if "GEMINI_API_KEY" not in os.environ:
+    os.environ["GEMINI_API_KEY"] = getpass.getpass("Enter Gemini API key: ")
+
+  executor = DatabaseAgent(
+      apiKey=os.environ["GEMINI_API_KEY"]
+  )
+
+  query = "I want to go hiking in the Garda region with medium difficulty and for up to 4 hours."
+  response = executor.run(query)
+  print(response)
